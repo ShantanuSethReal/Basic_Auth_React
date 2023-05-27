e3718fdbed20793dcc87711389737a7627cbbe46
ShantanuSethReal <117711288+ShantanuSethReal@users.noreply.github.com>
2023-05-27 12:39:58 +0530

    12:39

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 // import logo from './logo.svg';
-import './App.css';
-import SignUp from "./Components/SignUp"
-import Login from "./Components/Login"
-import Profile from './Components/Profile';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <>
-      <BrowserRouter basename='/Contest_27May'>
+      <BrowserRouter basename="/Contest_27May">
         <Navbar />
         <Routes>
-          <Route path="/" element={<SignUp />}></Route>
+          <Route exact path="/" element={<SignUp />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
         </Routes>
       </BrowserRouter>
-      
     </>
   );
 }
